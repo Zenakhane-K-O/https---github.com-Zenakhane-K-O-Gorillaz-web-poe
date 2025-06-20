557bb83e7a05195d305dabceb06a701dba256d49
Zenakhane-K-O <Zoni1160@gmail.com>
2025-06-20 10:08:23 +0000

    message

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,3 +59,14 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+function openLightbox(img) {
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+    lightbox.style.display = "block";
+    lightboxImg.src = img.src;
+    lightboxImg.alt = img.alt;
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
